184982fec6ff87544e3a4d6fe37f4d3d6e35b700
Christian Höltje <docwhat@gerf.org>
2018-07-24 16:59:26 -0400

    add cli argument handling

--- src/cli.ts
+++ src/cli.ts
@@ -1,20 +1,37 @@
 #!/usr/bin/env node
+import fs = require('fs')
 import stringify = require('json-stable-stringify')
 import process = require('process')
+import yargs = require('yargs')
+
+const argv = yargs
+  .strict()
+  .usage('$0 <cmd> [args]')
+  .epilog(
+    'For more information, see https://github.com/docwhat/json-canonicalizer'
+  )
+  .help('help')
+  .alias('h', 'help')
+  .wrap(yargs.terminalWidth()).argv
+
+const stream: NodeJS.ReadableStream = argv._[0]
+  ? fs.createReadStream(argv._[0])
+  : process.stdin
 
 const inputString: string[] = []
 
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read()
+stream.on('readable', () => {
+  const chunk = stream.read()
   if (chunk !== null) {
     inputString.push(`${chunk}`)
   }
 })
 
-process.stdin.on('end', () => {
+stream.on('end', () => {
   const data = JSON.parse(inputString.join(''))
   const opts = {
     space: 2,
   }
   process.stdout.write(stringify(data, opts))
+  process.stdout.write('\n')
 })
