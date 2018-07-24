dfb1a315eda7de05c98c9e9f39551caa2a98bd2a
Christian Höltje <docwhat@gerf.org>
2018-07-24 14:57:07 -0400

    first draft

---
+++ src/cli.ts
@@ -0,0 +1,19 @@
+import stringify = require('json-stable-stringify')
+import process = require('process')
+
+const inputString: string[] = []
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read()
+  if (chunk !== null) {
+    inputString.push(`${chunk}`)
+  }
+})
+
+process.stdin.on('end', () => {
+  const data = JSON.parse(inputString.join(''))
+  const opts = {
+    space: 2,
+  }
+  process.stdout.write(stringify(data, opts))
+})
